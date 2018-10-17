45e714c591f51ca73e6a11a5b94f843f5cca17f3
gargonmi <m.a.gargonx@gmail.com>
2018-10-17 19:56:25 +0200

    ejercicios clase 7

---
+++ miguelGarcia/public/clase7Ejercicio4.js
@@ -0,0 +1,29 @@
+
+function imprimirParesFor (){
+    for(i=2 ; i < 101; i+=2){
+    console.log(i);
+    }
+    }
+    
+    
+    function imprimirParesWhile (){
+    var i = 2;
+    
+        while(i < 101){
+        console.log(i);
+        i += 2;
+        }
+                            }
+    
+    
+    
+    function imprimirParesDoWhile (){
+    var i = 2;
+                                
+     do{
+    console.log(i);
+    i += 2 ;
+        }while(i < 101);
+                             }
+                                
+    
